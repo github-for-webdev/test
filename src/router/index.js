8ef1b1e840b0d37b35533d9480469c701a2811b7
github-for-webdev <steamprofile2020@gmail.com>
2022-03-07 22:37:14 +0300

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,46 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-// import Test from "@/views/Test.vue";
-// import  from "@/views/.vue";
-// import  from "@/views/.vue";
-// import  from "@/views/.vue";
-// import  from "@/views/.vue";
-// import  from "@/views/.vue"
+import HomePage from "@/views/HomePage.vue";
+import DagestanPage from "@/views/DagestanPage.vue";
+import TeriberkaPage from "@/views/TeriberkaPage.vue";
+import ElbrusPage from "@/views/ElbrusPage.vue";
+import СrimeaPage from "@/views/СrimeaPage.vue";
+import PolarPage from "@/views/PolarPage.vue";
+import AltayPage from "@/views/AltayPage.vue";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: HomePage,
+  },
+  {
+    path: "/dagestan",
+    component: DagestanPage,
+  },
+  {
+    path: "/teriberka",
+    component: TeriberkaPage,
+  },
+  {
+    path: "/elbrus",
+    component: ElbrusPage,
+  },
+  {
+    path: "/crimea",
+    component: СrimeaPage,
+  },
+  {
+    path: "/polar",
+    component: PolarPage,
+  },
+  {
+    path: "/altay",
+    component: AltayPage,
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
